7edc9df6c1053d4b2ace9b30f527b0e0caef5ed2
tapisky <jctapiador@gmail.com>
2012-12-10 23:21:58 +0100

    Upgraded to Unity 4 and made a new demo with some samples

--- Unity/Ejemplo1/Assets/BackgroundMusic.cs
+++ Unity/Ejemplo1/Assets/BackgroundMusic.cs
@@ -3,31 +3,142 @@ using System.Collections;
 
 public class BackgroundMusic : MonoBehaviour {
 	
+	private Rect labelRect;
+	private AudioSource snare;
+	private AudioSource kick;
+	private AudioSource hat;
+	private AudioSource pizzi1;
+	private AudioSource pizzi2;
+	private AudioSource pizzi3;
+	private AudioSource sample;
 	
-	float looptime = 0;
+	public CollisionBouncing nb;
 	
 	// Use this for initialization
 	void Start () {
-	
-		//looptime = audio.clip.length;
-		//audio.loop = true;
-		//audio.playOnAwake = false;
-		//audio.Play();
-		//Invoke("NextClip", looptime);
+		
+		labelRect = new Rect(0, 0, 200, 20);
+		
+		snare = GameObject.Find("snare").audio;
+		snare.volume = 0.2F;
+		
+		kick = GameObject.Find("kick").audio;
+		kick.volume = 0.0F;
+		
+		hat = GameObject.Find("hat").audio;
+		hat.volume = 0.0F;
+
+		pizzi1 = GameObject.Find("pizzi1").audio;
+		pizzi1.volume = 0.0F;
+		pizzi2 = GameObject.Find("pizzi2").audio;
+		pizzi2.volume = 0.0F;
+		pizzi3 = GameObject.Find("pizzi3").audio;
+		pizzi3.volume = 0.0F;
+				
+		nb = GetComponent<CollisionBouncing>();
+
+		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-		//print (looptime);
-		//looptime = audio.clip.length;
+	void LateUpdate ()
+	{
+		
+		if ( ((nb.numbounces % 4) == 0) && (nb.numbounces > 0)  || ( ( ((nb.numbounces % 4) >= 0) && ((nb.numbounces % 4) <= 1) ) && (nb.numbounces > 16)) )
+		{
+			StartCoroutine(fadein(kick,0.2F));
+		}
+		else
+		{
+			StartCoroutine(fadeout(kick));
+		}
+		
+		if ( (nb.numbounces > 8) && ( ((nb.numbounces % 4) == 0) || ((nb.numbounces % 4) == 1) ) )
+		{
+			StartCoroutine(fadein(hat,0.1F));
+		}
+		else
+		{
+			StartCoroutine(fadeout(hat));
+		}
+		//print ("Samples mod 22050: " + (hat.timeSamples % 22050));
+		
+		if (nb.numbounces > 20)
+		{
+			
+			StartCoroutine(playscheduled(pizzi1));
+		}
+		
+		
+		if (nb.numbounces > 40)
+		{
+			StartCoroutine(stopscheduled(pizzi1));
+			StartCoroutine(playscheduled(pizzi2));
+		}
+		
+		if (nb.numbounces == 60)
+		{
+			nb.numbounces = 15;
+			
+		}
 	}
 	
-	void NextClip() {
+	IEnumerator playscheduled(AudioSource audioobj)
+	{
 		
-		//audio.Stop();
-		//audio.Play ();
-		//Invoke("NextClip", looptime);
-		
+		yield return new WaitForSeconds(audioobj.clip.length - audioobj.time - 0.1F);
+		audioobj.volume = 0.08F;
+		yield return null;
 	}
+	
+	IEnumerator stopscheduled(AudioSource audioobj)
+	{
+		
+		yield return new WaitForSeconds(audioobj.clip.length - audioobj.time - 0.1F);
+		audioobj.volume = 0.0F;
+		yield return null;
+	}
+	
+	
+	IEnumerator fadeout(AudioSource audioobj)
+	{
+		/*while (audioobj.volume > 0.05F)
+		{
+			audioobj.volume = audioobj.volume - 0.00005F;	
+		}
+		*/
+		while ( (audioobj.timeSamples % 22050) < 20000)
+		{
+			yield return new WaitForSeconds(0.3F);
+		}
+		audioobj.volume = 0.0F;
+		yield return null;
+	}
+	
+	IEnumerator fadein(AudioSource audioobj, float vol)
+	{
+		/*
+		while (audioobj.volume < 0.2F)
+		{
+			audioobj.volume = audioobj.volume + 0.00005F;
+		}
+		*/
+		while ( (audioobj.timeSamples % 22050) < 20000)
+		{
+			yield return new WaitForSeconds(0.3F);
+		}
+		audioobj.volume = vol;
+		yield return null;
+	}
+	
+	void OnGUI()
+
+	{
+
+   		//GUI.Label(labelRect, "Bar: " + bar + "    Beat: " + beat + "    Meas: " + meas);
+		GUI.Label(labelRect, "Num. bounces: " + nb.numbounces);
+		
+
+	}
+	
 }
--- Unity/Ejemplo1/Assets/CollisionBouncing.cs
+++ Unity/Ejemplo1/Assets/CollisionBouncing.cs
@@ -3,45 +3,46 @@ using System.Collections;
 
 public class CollisionBouncing : MonoBehaviour {
 	
-	public AudioClip bounceclip;
-	public AudioClip bouncewall;
+	public AudioSource bounceclip;
+	public AudioSource bouncewall;
+	public int numbounces;
 	
+		
 	// Setup audio clips at the beginning
 	void Start () {
 		
- 		bounceclip = GameObject.Find("Ball_Bounce").audio.clip;
-		bouncewall = GameObject.Find("Platform_clip").audio.clip;
+ 		bounceclip = GameObject.Find("Ball_Bounce").audio;
+		bouncewall = GameObject.Find("Platform_clip").audio;
+		numbounces = 0;
 	
 	}
 	
 	public int collisionPower;
 	public GameObject bounceAgainst;
 	
-	void OnCollisionEnter(Collision collisionInfo) {
+	
+	void OnCollisionEnter(Collision collisionInfo) 
+	{
 		// play bounce sound if ball colliding against bouncer
 		if (collisionInfo.collider.name == "Bouncer"){
-			audio.clip = bounceclip;
-			audio.loop = false;
-			audio.pitch = Random.Range(0.75f,1.2f);
-			audio.Play();
+			bounceclip.loop = false;
+			bounceclip.pitch = Random.Range(0.75f,1.2f);
+			bounceclip.Play();
+			numbounces ++;
 		}
 		// play wall hit sound instead
 		else if ( (collisionInfo.collider.name == "Side1") || (collisionInfo.collider.name == "Side2") ){
-			audio.clip = bouncewall;
-			audio.loop = false;
-			audio.pitch = Random.Range(0.75f,1.4f);
-			audio.Play();
-		
-
-			print ("Aquí estamos");	
-			
+			bouncewall.loop = false;
+			bouncewall.pitch = Random.Range(0.75f,1.4f);
+			bouncewall.Play();	
 		}
 		//print("Collision detected");
-		print("Collision detected between " + this.name + " and " + collisionInfo.collider.name);
+		//print("Collision detected between " + this.name + " and " + collisionInfo.collider.name);
 		if (bounceAgainst.name == collisionInfo.collider.name){
 	        foreach (ContactPoint contact in collisionInfo.contacts) {
 				this.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * collisionPower);
 	        }
 		}
 	}
+	
 }
